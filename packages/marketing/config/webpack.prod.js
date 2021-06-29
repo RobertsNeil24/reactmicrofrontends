133cd41755db2fd77ddbf7addf7a104e4a03d388
Neil Roberts <nsr2412@gmail.com>
2021-06-29 14:21:49 +0200

    Github actions fix

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -5,6 +5,7 @@ const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
+// Webpack Prod configuration
 const prodConfig = {
     mode: 'production',
     output: {
